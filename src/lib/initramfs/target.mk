3c4fba765300c5d6aa144f3280d7adc303e3f216
Johannes Kliemann <Johannes.Kliemann@tu-dresden.de>
2017-11-02 09:46:43 +0100

    build initramfs binary from genode

--- src/lib/initramfs/target.mk
+++ src/lib/initramfs/target.mk
@@ -1,7 +1,19 @@
 TARGET = lx_init
-SRC_C  = init.c
-INC_DIR += /usr/include/\
-	   /usr/include/x86_64-linux-gnu/
-CUSTOM_CC = gcc
-CFLAGS = -static -march=native -std=gnu99
-LDFLAGS = 
+
+INITRAMFS = initramfs
+INITRAMFS_SRC_C = init.c
+
+EXT_OBJECTS += $(BUILD_BASE_DIR)/lib/initramfs/$(INITRAMFS)
+
+$(TARGET): $(INITRAMFS)
+
+$(INITRAMFS): $(INITRAMFS_SRC_C)
+	$(MSG_BUILD)$(INITRAMFS)
+	$(VERBOSE)gcc $^ -Ofast $(CC_MARCH) -Wall -W -Wextra -Werror -std=gnu99 -o $@ -Wl,-O3 -Wl,--as-needed -static
+
+clean_initramfs:
+	$(VERBOSE)rm -rf $(INITRAMFS)
+
+clean: clean_initramfs
+
+vpath init.c $(PRG_DIR)
