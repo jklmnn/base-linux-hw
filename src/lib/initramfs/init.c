64d3ddb113a9635a1de4310eece44a39149bf7b9
Johannes Kliemann <Johannes.Kliemann@tu-dresden.de>
2017-12-07 11:42:53 +0100

    print initramfs rom

--- src/lib/initramfs/init.c
+++ src/lib/initramfs/init.c
@@ -1,5 +1,29 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+void print_rom(void)
+{
+    printf("\n:initramfs: ROM modules:\n");
+    DIR *genode = opendir(".");
+    if(genode){
+        struct dirent* entry;
+        off_t offset = 0;
+        while((entry = readdir(genode))){
+            if(strcmp(entry->d_name, ".") && strcmp(entry->d_name, "..")){
+                struct stat st;
+                stat(entry->d_name, &st);
+                printf(" ROM: [%#08lx, %#08lx] %s\n", offset, st.st_size, entry->d_name);
+            }
+            offset += entry->d_off;
+        }
+    }
+    printf("\n");
+}
 
 int
 main (int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
@@ -10,6 +34,7 @@ main (int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
         perror("failed to chdir into /genode");
         return 1;
     }
+    print_rom();
     if(execve("core", &args, NULL)){
         perror("failed to start core");
         return 1;
