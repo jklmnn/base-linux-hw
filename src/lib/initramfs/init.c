0b41bc517c1185c6f46a7165caf7066ab063a332
Johannes Kliemann <Johannes.Kliemann@tu-dresden.de>
2018-01-11 16:15:12 +0100

    load hwio.ko kernel module to be used by genode

--- src/lib/initramfs/init.c
+++ src/lib/initramfs/init.c
@@ -1,10 +1,38 @@
 #include <unistd.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+
+int insmod(char *path)
+{
+    int fd;
+    struct stat st;
+    void *image;
+
+    fd = open(path, O_RDONLY);
+    if(fd == -1){
+        return 1;
+    }
+    fstat(fd, &st);
+
+    image = malloc(st.st_size);
+    read(fd, image, st.st_size);
+    close(fd);
+
+    return syscall(__NR_init_module, image, st.st_size, "");
+}
 
 int
 main (int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
 {
     char *args = "core";
+    printf("loading hwiodev module\n");
+    if(insmod("/hwio.ko")){
+        perror("insmod");
+    }
     printf("loading Genode on Linux\n");
     if (chdir("/genode")){
         perror("failed to chdir into /genode");
