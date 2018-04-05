aeb1667c0addc21a67daaab7a073939482d5ef79
Johannes Kliemann <Johannes.Kliemann@tu-dresden.de>
2018-04-05 16:50:11 +0200

    parse multiple binaries at once

--- tool/sloccount.sh
+++ tool/sloccount.sh
@@ -1,31 +1,37 @@
 #!/bin/sh
 
 echo Generating hash of $1 ...
-HASH=$(md5sum $1 | cut -d" " -f1)
-FILE=$(echo $1 | sed "s/.*\/\(.*\)/\1/g")
+HASH=$(md5sum $@ | cut -d" " -f1 | sort | uniq | md5sum | cut -d " " -f1)
 WORKDIR=/tmp/$HASH
-
+mkdir -p $WORKDIR
 echo Working in $WORKDIR
 
-mkdir -p $WORKDIR
+for binary in $@
+do
 
-if [ ! -e $WORKDIR/$FILE.txt ]
-then
-    echo Generating objectdump ...
-    objdump -lSd $1 > $WORKDIR/$FILE.txt
-else
-    echo Using cached objectump
-fi
+    FILE=$(echo $binary | sed "s/.*\/\(.*\)/\1/g")
 
-echo Parsing objectdump ...
-cat $WORKDIR/$FILE.txt | \
-    grep ".*\.[chS]\(c\?\|\(pp\)\?\):[0-9]\+" | \
-    sed "s/\(.*\):[0-9]\+.*/\1/g" | \
-    sort | \
-    uniq | \
-    xargs md5sum | \
-    sed "s/\([0-9a-f]*\) *\(.*\)\/\(.*\)/\2\/\3 $(echo $WORKDIR | sed 's/\//\\\//g')\/src\/\/\1-\3/g" | \
-    xargs -L1 -I % sh -c 'install -D %; printf "$(echo % | sed "s/.*\([0-9a-f]\{32\}\).*/\1/g" | cut -b1-6)\r"'
+    echo Processing $FILE ...
+
+    if [ ! -e $WORKDIR/$FILE.txt ]
+    then
+        echo Generating objectdump ...
+        objdump -lSd $binary > $WORKDIR/$FILE.txt
+    else
+        echo Using cached objectump
+    fi
+
+    echo Parsing objectdump ...
+    cat $WORKDIR/$FILE.txt | \
+        grep ".*\.[chS]\(c\?\|\(pp\)\?\):[0-9]\+" | \
+        sed "s/\(.*\):[0-9]\+.*/\1/g" | \
+        sort | \
+        uniq | \
+        xargs md5sum | \
+        sed "s/\([0-9a-f]*\) *\(.*\)\/\(.*\)/\2\/\3 $(echo $WORKDIR | sed 's/\//\\\//g')\/src\/\/\1-\3/g" | \
+        xargs -L1 -I % sh -c 'install -D %; printf "$(echo % | sed "s/.*\([0-9a-f]\{32\}\).*/\1/g" | cut -b1-6)\r"'
+
+done
 
 mkdir $WORKDIR/slocdata
 sloccount --datadir $WORKDIR/slocdata $WORKDIR/src
